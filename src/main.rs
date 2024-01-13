3cb662bcf517ed6ba602fc32b26606b7431fa3db
SpruceWareDev <144385663+TomSJD@users.noreply.github.com>
2024-01-13 23:44:12 +0000

    Added remove functionality
    You can now remove users by username.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::stdin;
 
-const OPTIONS: [&str; 3] = ["1. Add User", "2. View All Users", "3. Exit"];
+const OPTIONS: [&str; 4] = ["1. Add User", "2. View All Users", "3. Remove User", "4. Exit"];
 
 struct User {
     username: String,
@@ -24,7 +24,8 @@ fn main() {
         match input.as_str() {
             "1" => add_user(&mut users),
             "2" => display_all_users(&users),
-            "3" => running = false,
+            "3" => remove_user(&mut users),
+            "4" => running = false,
             _ => println!("Not a valid option"),
         }
     }
@@ -45,6 +46,19 @@ fn add_user(users: &mut Vec<User>) {
     println!("Enter an email.");
     let email: String = read_user_input();
     users.push(User { username, email });
+}
+
+fn remove_user(users: &mut Vec<User>) {
+    println!("Enter a username to remove.");
+    let username: String = read_user_input();
+    for (i, user) in &mut users.iter().enumerate() {
+        if &user.username == &username {
+            users.swap_remove(i);
+            println!("Removed user: {}", &username);
+            return;
+        }
+    }
+    println!("User does not exist!");
 }
 
 fn display_all_users(users: &Vec<User>) {
